45e471d737fe2cffbd10fa747667b4b42c949ee0
Preksha <ranapreksha8@gmail.com>
2025-04-04 18:03:17 +0530

    Added Changes

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, ref } from 'vue'
+
+const firstName = "Preksha";
+
+const lastName = "<p style=\"color:blue\">Rana</p>";
+
+const objAttribute = {
+  id: 'container',
+  class: 'wrapper',
+  style: 'background-color:pink; color:white; margin-top:5px;padding:10px; border-radius:10px;width:200px;'
+};
+
+const count = ref(0);
+
+function increment() {
+  count.value++
+};
+
+const classObj = {
+  active: true,
+  'text-danger': false
+};
+
+const styleObj = {
+  color: 'red',
+  fontSize: '20px'
+};
+
+const obj = reactive({
+  name: 'Preksha',
+  age: 22,
+  city: 'Delhi'
+});
+
+function showData() {
+  obj.name = 'Prekshii';
+};
+
+const awesome = ref(true);
+
+const parentMessage = ref('Parent')
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+
+function greet(msg) {
+  alert(msg);
+}
+
+const message = ref('Hello Preksha!');
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>Hello, <span>{{ firstName }}</span><span v-html="lastName"></span></h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <button :disabled="true">Click Me</button>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <div v-bind="objAttribute">Hello World!</div>
+
+  <button v-on:click="increment">Increment by {{ count }}</button>
+
+  <p :class="classObj" :style="styleObj">This is a paragraph</p>
+
+  <button @click="showData">Show Data</button>
+  <p>{{ obj.name }} {{ obj.age }} {{ obj.city }}</p>
+
+  <button @click="awesome = !awesome">Toggle</button>
+
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
+
+  <li v-for="(item, index) in items">
+    {{ parentMessage }} - {{ index }} - {{ item.message }}
+  </li>
+  <br>
+  <li v-for="(value, key) in obj">{{ key }}:{{ value }}</li>
+  <br>
+  <button @click="greet('Hello from preksha')">Greet</button><br><br>
+  <button @click="greet(`Hello from ${firstName}`)">Greet</button>
+
+  <p>Message is: {{ message }}</p>
+  <input v-model="message" placeholder="edit me" />
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
